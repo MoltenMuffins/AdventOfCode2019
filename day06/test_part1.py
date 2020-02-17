8b130176e851260931712e4adf869ef31784d9a5
Evan Chong <evan.tkchong@gmail.com>
2020-02-17 15:53:58 +0800

    Complete day 6 part 1

---
+++ day06/test_part1.py
@@ -0,0 +1,33 @@
+# !/usr/bin/python
+from solution_part1 import (generate_orbit_map,
+                            run_count_checksum)
+
+sum_list = [
+    'COM)B',
+    'B)C',
+    'C)D',
+    'D)E',
+    'E)F',
+    'B)G',
+    'G)H',
+    'D)I',
+    'E)J',
+    'J)K',
+    'K)L'
+]
+
+test_cases = {
+    42: sum_list
+}
+
+def check_cases(cases:dict):
+    for key, value in cases.items():
+        orbits = generate_orbit_map(value)
+        checksum = run_count_checksum(orbits)
+        assert checksum == key
+    print('👍')
+
+if __name__ == "__main__":
+    check_cases(test_cases)
+
+    
