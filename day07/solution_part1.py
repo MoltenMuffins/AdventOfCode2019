901a0f3dffd5b37f6affc2f0c2327791e6e81545
Evan Chong <evan.tkchong@gmail.com>
2020-02-20 01:36:20 +0800

    init day 7

---
+++ day07/solution_part1.py
@@ -0,0 +1,7 @@
+# !/usr/bin/python
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+puzzle_input_path = os.path.join(dir_path, 'puzzle_input.txt')
+
+if __name__ == "__main__":
+    pass
